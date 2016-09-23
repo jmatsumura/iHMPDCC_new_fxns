ef751deb0aca54fe36c2e82a26278679857471dd
James Matsumura <james.matsumura@gmail.com>
2016-09-23 16:09:45 -0400

    node/edge seems to be working. time to get Neo4j to actually find this stuff

--- GQL_OSDF/schema.py
+++ GQL_OSDF/schema.py
@@ -19,8 +19,11 @@ from neo4j_query import get_project
 # Each node will have what makes it unique at the top and then generic/default values shared
 # across all nodes following that. This will be restructured once what fields are absolutely
 # essential are determined. 
+#
+# If edges are present between nodes, then these connections will be specified inbetween the
+# different node types. 
 
-class Project(graphene.ObjectType):
+class Project(graphene.ObjectType): # node
 
     class Meta:
         interfaces = [Node]
@@ -28,28 +31,13 @@ class Project(graphene.ObjectType):
     subtype = graphene.String()
     name = graphene.String()
     description = graphene.String()
-    # Defaults
-    _id = graphene.ID()
+    _id = graphene.ID() # Defaults
     nodeType = graphene.String()
     aclRead = graphene.String()
     aclWrite = graphene.String()
 
 
-class Sample(graphene.ObjectType):
-
-    class Meta:
-        interfaces = [Node]
-
-    fmaBodySite = graphene.String()
-    # Defaults
-    _id = graphene.ID()
-    nodeType = graphene.String()
-    aclRead = graphene.String()
-    aclWrite = graphene.String()
-
-# Cover edges here, note that we probably need more than just what is provided by iHMP if we want
-# to group things like Projects under one branch.
-class PartOf(Connection):
+class PartOf(Connection): # edge
 
     class Meta:
         node = Project
@@ -58,9 +46,49 @@ class PartOf(Connection):
         rel_type = 'part_of'
 
 
-class Query(graphene.ObjectType):
+class Study(graphene.ObjectType): # node
+
+    class Meta:
+        interfaces = [Node]
+    projects = relay.ConnectionField(PartOf) # this expects a list of the node objects
+
+    subtype = graphene.String()
+    center = graphene.String()
+    contact = graphene.String()
+    name = graphene.String()
+    description = graphene.String()
+    _id = graphene.ID() # Defaults
+    nodeType = graphene.String()
+    aclRead = graphene.String()
+    aclWrite = graphene.String()
+
+
+class ParticipatesIn(Connection): # edge
+
+    class Meta:
+        node = Study
+
+    class Edge:
+        rel_type = 'participates_in'
+
+
+class Sample(graphene.ObjectType): # node
+
+    class Meta:
+        interfaces = [Node]
+    studies = relay.ConnectionField(ParticipatesIn)
+
+    fmaBodySite = graphene.String()
+    _id = graphene.ID() # Defaults
+    nodeType = graphene.String()
+    aclRead = graphene.String()
+    aclWrite = graphene.String()
+
+
+class Query(graphene.ObjectType): # grab everything! 
 
     project = graphene.Field(Project)
+    study = graphene.Field(Study)
     sample = graphene.Field(Sample)
     node = relay.Node.Field() # get single Node if needed
 
@@ -68,8 +96,11 @@ class Query(graphene.ObjectType):
     def resolve_project(self, args, context, info):
         return Project(id='123j', nodeType='project', aclRead='ihmp', aclWrite='ihmp', subtype='hmp', name='Human Microbiome Project(HMP)', description='asdlfj')
 
+    def resolve_study(self, args, context, info):
+        return Study(id='123j', nodeType='project', aclRead='ihmp', aclWrite='ihmp', subtype='1', center='2', contact='yes', name='no', description='maybe', projects=[])
+        
     def resolve_sample(self, args, context, info):
-        return Sample(id='123j', nodeType='project', aclRead='ihmp', aclWrite='ihmp', fmaBodySite='head')
+        return Sample(id='123j', nodeType='project', aclRead='ihmp', aclWrite='ihmp', fmaBodySite='head', studies=[])       
 
 # As noted above, going to hit Neo4j once and get everything then let GQL 
 # do its magic client side to return the values that the user wants. 
