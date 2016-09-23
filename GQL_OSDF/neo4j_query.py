54c74dc627ef7a678f2e4e355352516fb042c2eb
James Matsumura <james.matsumura@gmail.com>
2016-09-23 17:00:30 -0400

    need to just accommodate pulling all, not one at a time

--- GQL_OSDF/neo4j_query.py
+++ GQL_OSDF/neo4j_query.py
@@ -1,29 +1,30 @@
-Project = {}
-DNAPrep16s = {}
-RawSeqSet16s = {}
-Sample = {}
-Study = {}
-Subject = {}
-TrimmedSeqSet16s = {}
-Visit = {}
-
 # Using py2neo 3 not 2
 from py2neo import Graph
 from pandas import DataFrame
-import numpy
+from schema import Project, Study, Sample
+
 graph = Graph("http://localhost:7474/db/data/")
 
 # Example ES query+result
-#es_result = graph.run("CALL ga.es.queryNode('{\"query\":{\"match\":{\"name\":\"iHMP\"}}}') YIELD node return node").data()
-#print(dumps(es_result))
+#print(graph.run("CALL ga.es.queryNode('{\"query\":{\"match\":{\"name\":\"iHMP\"}}}') YIELD node return node").data())
 
 # Example normal Cypher query+result
-# The below returns an array where each element is a node+properties requested
-#reg_result = graph.data("MATCH (n:Project) RETURN n.name")
+#print(graph.data("MATCH (n:Project) RETURN n.name"))
 
-def get_project():
-    result = graph.data("MATCH (n:Project) RETURN n")
-    return '123j'#(_id='123j', nodeType='project', aclRead='ihmp', aclWrite='ihmp', subtype='hmp', name='Human Microbiome Project(HMP)', description='asdlfj')
+attr = "node_type"
+val = "project"
+test = "CALL ga.es.queryNode('{\"query\":{\"match\":{\"%s\":\"%s\"}}}') YIELD node return node" % (attr, val)
+
+print(graph.data(test))
+
+def get_project(): # retrieve a single project (done once a project is decided upon)
+    #from schema import Project # need to figure out circular dependency work around
+    result = graph.data(test)
+    print 'dummy'#(_id='123j', nodeType='project', aclRead='ihmp', aclWrite='ihmp', subtype='hmp', name='Human Microbiome Project(HMP)', description='asdlfj')
+
+
+#def get_all_projects(): # retrieve all projects
+
 
 #def get_dnaprep16s(character):
    
