8bd50059df9d74bd510857cee73145f5b170f8df
James Matsumura <james.matsumura@gmail.com>
2016-10-04 12:08:47 -0400

    adding in table_schema

---
+++ GQL_OSDF/table_schema.py
@@ -0,0 +1,28 @@
+import graphene
+from graphene import relay
+from models import Pagination, FileHits, Aggregations, get_buckets, get_hits
+
+# Can preload aggregate. Note that this function needs to be changed up a bit for files counts since
+# it needs to pull ALL nodes that are tied to some file and count those unique groups. Should be easy
+# enough, just match by the relevant edges. Simplified for now. 
+dt = get_buckets("RawSeqSet16s.node_type","no")
+df = get_buckets("RawSeqSet16s.format","no")
+
+class Query(graphene.ObjectType):
+
+    pagination = graphene.Field(Pagination)
+    hits = graphene.List(FileHits)
+    aggregations = graphene.Field(Aggregations)
+
+    def resolve_pagination(self, args, context, info):
+        return Pagination(count=0, sort="case_id.raw:asc", fromNum=1, page=1, total=3, pages=1, size=0)
+
+    def resolve_hits(self, args, context, info):
+        return get_file_hits()
+
+    def resolve_aggregations(self, args, context, info):
+        return Aggregations(dataType=dt, dataFormat=df)
+
+# As noted above, going to hit Neo4j once and get everything then let GQL 
+# do its magic client side to return the values that the user wants. 
+table_schema = graphene.Schema(query=Query)
