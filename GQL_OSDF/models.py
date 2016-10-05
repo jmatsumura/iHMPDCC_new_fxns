77d189508a2f0a18097d15aa7bd6cd93780817ee
James Matsumura <james.matsumura@gmail.com>
2016-10-05 17:07:52 -0400

    no longer including deprecated section. laying framework for route to take with getting file. need to polish indiv_files_schema.py before committing

--- GQL_OSDF/models.py
+++ GQL_OSDF/models.py
@@ -228,172 +228,24 @@ def get_file_hits():
         hits.append(cur_file2)       
     return hits
 
+# The following section is needed to pull data from the different file types/nodes.
+# How it aims to work is given a file ID, it will check Neo4j for the type then use
+# this knowledge to build a smarter Cypher query that will return the relevant data.
+# The logic here will be much easier to follow then writing one huge catch-all Neo4j
+# query and then parsing it afterwards.
+def get_16s_raw_seq_set(id):
+    return id
 
+def get_16s_trimmed_seq_set(id):
+    return id
 
-##############
-# DEPRECATED #
-##############
+options = {'16s_raw_seq_set': get_16s_raw_seq_set,
+    '16s_trimmed_seq_set': get_16s_trimmed_seq_set,
+}
 
-class Defaults(graphene.Interface):
-    ID = graphene.List(graphene.String)
-    #n odeType is also redundant due to how the GQL statements are named (e.g. project root of gql query
-    # will always have nodeType == 'project')
-    #nodeType = graphene.List(graphene.String)
-
-    # These next two are mostly needed for authentication purposes (if needed). Read is really only
-    # relevant to the users while write is more of a backend thing. No need for write until proven otherwise.
-    # If we want security, should create two seperate GQL endpoints (one which only presents the aclRead=='read')
-    #aclRead = graphene.List(graphene.String) 
-    #aclWrite = graphene.List(graphene.String)
-
-class Project_old(graphene.ObjectType): # used to be named Project and accommodated all the Graphene objects below
-    class Meta:
-        interfaces = (Defaults, )
-    subtype = graphene.List(graphene.String)
-    name = graphene.List(graphene.String)
-    description = graphene.List(graphene.String)
-
-class Study(graphene.ObjectType):
-    class Meta:
-        interfaces = (Defaults, )
-    project = graphene.List(Project)
-    subtype = graphene.List(graphene.String)
-    center = graphene.List(graphene.String)
-    contact = graphene.List(graphene.String)
-    name = graphene.List(graphene.String)
-    description = graphene.List(graphene.String)
-    partOf = graphene.List(graphene.String) # part of what project
-
-# Below are functions to extract all the data related to a particular node that might
-# be worth searching. Since this is meant to populate auto-complete text, the fields
-# where there are likely redundant (will only add to the clutter of options) values
-# are checked for in any of the 'if' statements.
-
-def get_project(): # retrieve all project node related data
-    idl, subtypel, namel, descriptionl = ([] for i in range(4)) # lists of each relevant query property
-    res = build_basic_query("node_type", "project", False)
-    for x in range(0,len(res)):
-        idl.append(res[x]['n']['id']) # need to switch ALL to 'id' or '_id'
-        subtypel.append(res[x]['n']['subtype'])
-        namel.append(res[x]['n']['name'])
-        descriptionl.append(res[x]['n']['description'])
-    return Project(ID=idl, subtype=subtypel, name=namel, description=descriptionl)
-
-def get_study():
-    idl, subtypel, centerl, contactl, namel, descriptionl, partOfl = ([] for i in range(7))
-    res = build_basic_query("null", "Study", ["Project","PART_OF"])
-    for x in range(0,len(res)):
-        idl.append(res[x]['b']['_id'])
-        if res[x]['b']['subtype'] is not None:
-            if res[x]['b']['subtype'] not in subtypel:
-                subtypel.append(res[x]['b']['subtype'])
-        if res[x]['b']['center'] not in centerl: centerl.append(res[x]['b']['center'])
-        if res[x]['b']['contact'] not in contactl: contactl.append(res[x]['b']['contact'])
-        namel.append(res[x]['b']['name'])
-        descriptionl.append(res[x]['b']['description'])
-        if res[x]['link'] not in partOfl: partOfl.append(res[x]['link'])
-    return Study(ID=idl, subtype=subtypel, center=centerl, contact=contactl, name=namel, description=descriptionl, partOf=partOfl)
-
-def get_subject():
-    idl, racel, genderl, randSubjectIdl, participatesInl = ([] for i in range(5))
-    res = build_basic_query("null", "Subject", ["Study","PARTICIPATES_IN"])
-    for x in range(0,len(res)):
-        idl.append(res[x]['b']['_id'])
-        if res[x]['b']['race'] is not None:
-            if res[x]['b']['race'] not in racel:
-                racel.append(res[x]['b']['race'])
-        if res[x]['b']['gender'] not in genderl: genderl.append(res[x]['b']['gender'])
-        randSubjectIdl.append(res[x]['b']['rand_subject_id'])
-        if res[x]['link'] not in participatesInl: participatesInl.append(res[x]['link'])
-    return Subject(ID=idl, race=racel, gender=genderl, randSubjectId=randSubjectIdl, participatesIn=participatesInl)
-
-def get_visit():
-    idl, datel, intervall, visitIdl, clinicIdl, visitNumberl, byl = ([] for i in range(7))
-    res = build_basic_query("null", "Visit", ["Subject","BY"])
-    for x in range(0,len(res)):
-        idl.append(res[x]['b']['_id'])
-        if res[x]['b']['date'] is not None: 
-            if res[x]['b']['date'] not in datel:
-                datel.append(res[x]['b']['date'])
-        if res[x]['b']['interval'] not in intervall: intervall.append(res[x]['b']['interval'])
-        visitIdl.append(res[x]['b']['visit_id'])
-        clinicIdl.append(res[x]['b']['clinic_id'])
-        if res[x]['b']['visit_number'] not in visitNumberl: visitNumberl.append(res[x]['b']['visit_number'])
-        if res[x]['link'] not in byl: byl.append(res[x]['link'])
-    return Visit(ID=idl, date=datel, interval=intervall, visitId=visitIdl, clinicId=clinicIdl, visitNumber=visitNumberl, by=byl)
-
-def get_sample():
-    idl, fmaBodySitel, collectedDuringl = ([] for i in range(3))
-    res = build_basic_query("null", "Sample", ["Visit","COLLECTED_DURING"])
-    for x in range(0,len(res)):
-        idl.append(res[x]['b']['_id'])
-        if res[x]['b']['fma_body_site'] not in fmaBodySitel:
-            if res[x]['b']['fma_body_site'] != "":
-                fmaBodySitel.append(res[x]['b']['fma_body_site'])
-        if res[x]['link'] not in collectedDuringl: collectedDuringl.append(res[x]['link'])
-    return Sample(ID=idl, fmaBodySite=fmaBodySitel, collectedDuring=collectedDuringl)
-
-def get_dnaprep16s():
-    idl, prepIdl, libLayoutl, storageDurationl, subtypel, ncbiTaxonIdl, sequencingCenterl, commentl, libSelectionl, preparedFroml = ([] for i in range(10))
-    res = build_basic_query("null", "DNAPrep16s", ["Sample","PREPARED_FROM"])
-    for x in range(0,len(res)):
-        idl.append(res[x]['b']['_id'])
-        prepIdl.append(res[x]['b']['prep_id'])
-        if res[x]['b']['lib_layout'] not in libLayoutl: libLayoutl.append(res[x]['b']['lib_layout'])
-        if res[x]['b']['storage_duration'] not in storageDurationl: storageDurationl.append(res[x]['b']['storage_duration'])
-        if res[x]['b']['subtype'] not in subtypel: subtypel.append(res[x]['b']['subtype'])
-        if res[x]['b']['ncbi_taxon_id'] not in ncbiTaxonIdl: ncbiTaxonIdl.append(res[x]['b']['ncbi_taxon_id'])
-        if res[x]['b']['sequencing_center'] not in sequencingCenterl: sequencingCenterl.append(res[x]['b']['sequencing_center'])
-        if res[x]['b']['lib_layout'] not in libLayoutl: libLayoutl.append(res[x]['b']['lib_layout'])
-        if res[x]['b']['comment'] is not None: 
-            if res[x]['b']['comment'] not in commentl:
-                commentl.append(res[x]['b']['comment'])
-        if res[x]['b']['lib_selection'] not in libSelectionl: libSelectionl.append(res[x]['b']['lib_selection'])
-        if res[x]['link'] not in preparedFroml: preparedFroml.append(res[x]['link'])
-    return DNAPrep16s(ID=idl, prepId=prepIdl, libLayout=libLayoutl, storageDuration=storageDurationl, subtype=subtypel, ncbiTaxonId=ncbiTaxonIdl, sequencingCenter=sequencingCenterl, comment=commentl, libSelection=libSelectionl, preparedFrom=preparedFroml)
-
-def get_rawseqset16s():
-    idl, formatDocl, studyl, expLengthl, formatl, seqModell, seqTypel, sizel, subtypel, commentl, sequencedFroml = ([] for i in range(11))
-    res = build_basic_query("null", "RawSeqSet16s", ["DNAPrep16s","SEQUENCED_FROM"])
-    for x in range(0,len(res)):
-        idl.append(res[x]['b']['_id'])
-        if res[x]['b']['format_doc'] is not None: 
-            if res[x]['b']['format_doc'] not in formatDocl:
-                formatDocl.append(res[x]['b']['format_doc'])
-        if res[x]['b']['study'] not in studyl: studyl.append(res[x]['b']['study'])
-        if res[x]['b']['exp_length'] not in expLengthl: expLengthl.append(res[x]['b']['exp_length'])
-        if res[x]['b']['format'] not in formatl: formatl.append(res[x]['b']['format'])
-        if res[x]['b']['seq_model'] is not None: 
-            if res[x]['b']['seq_model'] not in seqModell:
-                seqModell.append(res[x]['b']['seq_model'])
-        if res[x]['b']['sequence_type'] is not None:
-            if res[x]['b']['sequence_type'] not in seqTypel: 
-                seqTypel.append(res[x]['b']['sequence_type'])
-        if res[x]['b']['size'] not in sizel: sizel.append(res[x]['b']['size'])
-        if res[x]['b']['subtype'] not in subtypel: subtypel.append(res[x]['b']['subtype'])
-        if res[x]['b']['comment'] is not None: 
-            if res[x]['b']['comment'] not in commentl:
-                commentl.append(res[x]['b']['comment'])
-        if res[x]['link'] not in sequencedFroml: sequencedFroml.append(res[x]['link'])
-    return RawSeqSet16s(ID=idl, formatDoc=formatDocl, study=studyl, expLength=expLengthl, format=formatl, seqModel=seqModell, seqType=seqTypel, size=sizel, subtype=subtypel, comment=commentl, sequencedFrom=sequencedFroml)
-
-def get_trimmedseqset16s():
-    idl, formatDocl, studyl, formatl, seqTypel, sizel, subtypel, commentl, computedFroml = ([] for i in range(9))
-    res = build_basic_query("null", "TrimmedSeqSet16s", ["RawSeqSet16s","COMPUTED_FROM"])
-    for x in range(0,len(res)):
-        idl.append(res[x]['b']['_id'])
-        if res[x]['b']['format_doc'] is not None: 
-            if res[x]['b']['format_doc'] not in formatDocl:
-                formatDocl.append(res[x]['b']['format_doc'])
-        if res[x]['b']['study'] not in studyl: studyl.append(res[x]['b']['study'])
-        if res[x]['b']['format'] not in formatl: formatl.append(res[x]['b']['format'])
-        if res[x]['b']['sequence_type'] is not None:
-            if res[x]['b']['sequence_type'] not in seqTypel: 
-                seqTypel.append(res[x]['b']['sequence_type'])
-        if res[x]['b']['size'] not in sizel: sizel.append(res[x]['b']['size'])
-        if res[x]['b']['subtype'] not in subtypel: subtypel.append(res[x]['b']['subtype'])
-        if res[x]['b']['comment'] is not None: 
-            if res[x]['b']['comment'] not in commentl:
-                commentl.append(res[x]['b']['comment'])
-        if res[x]['link'] not in computedFroml: computedFroml.append(res[x]['link'])
-    return TrimmedSeqSet16s(ID=idl, formatDoc=formatDocl, study=studyl, format=formatl, seqType=seqTypel, size=sizel, subtype=subtypel, comment=commentl, computedFrom=computedFroml)
+def get_file_data(file_id):
+    cquery = "MATCH (n) WHERE n._id=\"%s\" RETURN n.node_type AS type" % (file_id)
+    res = graph.data(cquery)
+    node = res[0]['type']
+    final_res = options[node](file_id)
+    return final_res
