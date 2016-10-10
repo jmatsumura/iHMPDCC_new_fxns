ec489779ae77b95b750078fce7fa0df5dc10b4df
James Matsumura <james.matsumura@gmail.com>
2016-10-10 11:41:54 -0400

    added list handling for advanced query. need to build a separate builder for facet only

--- GQL_OSDF/query.py
+++ GQL_OSDF/query.py
@@ -5,11 +5,19 @@ from py2neo import Graph
 # the graph entirely and use filters to return a subset of the total traversal. 
 base_query = "MATCH (pro:Project)<-[:PART_OF]-(stu:Study)<-[:PARTICIPATES_IN]-(sub:Subject)<-[:BY]-(vis:Visit)<-[:COLLECTED_DURING]-(sam:Sample)<-[:PREPARED_FROM]-(pf)<-[:SEQUENCED_FROM]-(sf)<-[:COMPUTED_FROM]-(cf) WHERE "
 
-# test strings
-tstr = '{"op":"and","content":[{"op":"AND","content":[{"op":"in","content":{"field":"cases.Project.name","value":["Human Microbiome Project (HMP)"]}},{"op":"OR","content":[{"op":"=","content":{"field":"cases.Sample.fma_body_site","value":"right cubital fossa [FMA:39849]"}},{"op":"=","content":{"field":"cases.Sample.fma_body_site","value":"Gingiva [FMA:59762]"}}]}]}]}'
-tstr2 = '{"op":"and","content":[{"op":"in","content":{"field":"cases.Project.name","value":["Human Microbiome Project (HMP)"]}}]}'
+# test strings with roughly increasing complexity
+tstr = '{"op":"and","content":[{"op":"in","content":{"field":"cases.Project.name","value":["Human Microbiome Project (HMP)"]}}]}'
+tstr2 = '{"op":"and","content":[{"op":"AND","content":[{"op":"in","content":{"field":"cases.Project.name","value":["Human Microbiome Project (HMP)"]}},{"op":"OR","content":[{"op":"=","content":{"field":"cases.Sample.fma_body_site","value":"right cubital fossa [FMA:39849]"}},{"op":"=","content":{"field":"cases.Sample.fma_body_site","value":"Gingiva [FMA:59762]"}}]}]}]}'
 tstr3 = '{"op":"and","content":[{"op":"OR","content":[{"op":"=","content":{"field":"cases.project.disease_type","value":"Acute Myeloid Leukemia"}},{"op":"OR","content":[{"op":"=","content":{"field":"cases.project.name","value":"Neuroblastoma"}},{"op":"=","content":{"field":"cases.case_id","value":"0004d251-3f70-4395-b175-c94c2f5b1b81"}}]}]}]}'
 tstr4 = '{"op":"and","content":[{"op":"OR","content":[{"op":"=","content":{"field":"cases.project.disease_type","value":"Acute Myeloid Leukemia"}},{"op":"OR","content":[{"op":"=","content":{"field":"cases.project.name","value":"Neuroblastoma"}},{"op":"OR","content":[{"op":"=","content":{"field":"cases.case_id","value":"0004d251-3f70-4395-b175-c94c2f5b1b81"}},{"op":"=","content":{"field":"cases.demographic.ethnicity","value":"not reported"}}]}]}]}]}'
+tstr5 = '{"op":"and","content":[{"op":"in","content":{"field":"cases.project.primary_site","value":["Kidney","Brain","Nervous System"]}}]}'
+tstr6 = '{"op":"and","content":[{"op":"in","content":{"field":"cases.project.primary_site","value":["Kidney","Brain","Nervous System"]}},{"op":"in","content":{"field":"cases.project.program.name","value":["TCGA"]}}]}'
+tstr7 = '{"op":"and","content":[{"op":"AND","content":[{"op":"in","content":{"field":"cases.ProjectName","value":["Human Microbiome Project (HMP)","iHMP"]}},{"op":"=","content":{"field":"cases.SampleFmabodysite","value":"Vagina [FMA:19949]"}}]}]}'
+
+comp_ops = ["=",">",">=","<","<=","!=","EXCLUDE","IN","in","IS","NOT"] # only found in advanced search
+comp_ops2 = ["AND","OR"] # separate group to delineate when to combine left/right halves of string
+comps = set(comp_ops)
+comps2 = set(comp_ops2)
 
 # This is a recursive function originally used to traverse and find the depth 
 # of nested JSON. Now used to traverse the op/filters query from GDC and 
@@ -34,21 +42,20 @@ def get_depth(x, arr):
         return max(get_depth(a, arr) for a in x)
     return arr # give the array back after traversal is complete
 
-comp_ops = ["=",">",">=","<","<=","!=","EXCLUDE","IN","in","IS","NOT"]
-comp_ops2 = ["AND","OR"]
-comps = set(comp_ops)
-comps2 = set(comp_ops2)
-skip_me = set()
+def build_facet_where(inp): # fxn to build Cypher based on facet search
+    return 'hi'
 
-def build_where(inp):
+def build_advanced_where(inp): # fxn to build Cypher based on advanced search
+    skip_me = set()
     lstr, rstr = ("" for i in range(2)) # right/left strings to combine
-    # Makes more sense to build up than it is to build down. Note that this range
-    # truncates the given JSON and removes the initial "and" as I'm not seeing
-    # the purpose of it at the moment. 
+    # Makes more sense to build up than it is to build down.
     for x in reversed(range(1,len(inp))):  
         if x in skip_me: # pass over elements we know are already consumed
             pass
         elif inp[x-2] in comps: # case to build comparison statement
+            if inp[x-2] == "in": # need to add brackets for Cypher if list present
+                inp[x] = "[%s]" % (inp[x])
+
             if lstr == "":
                 lstr = "%s %s %s" % (inp[x-1],inp[x-2],inp[x])
             else:
@@ -57,14 +64,14 @@ def build_where(inp):
         else: # process the overarching AND/OR of the WHERE
             if inp[x] in comps2: # check for clarity
                 rstr = "%s %s %s" % (lstr,inp[x],rstr)
-                lstr = "" # reset, rstr will now be built upon
-
-    print rstr
+                lstr = "" # reset, rstr will be built upon
+    return rstr
 
 arr = []    
 fs = json.loads(tstr4) # from string to json
 where = get_depth(fs, arr)
-build_where(where)
+x = build_advanced_where(where)
+print(x)
 
 # Need a function for building the RETURN clause and then put together the final query
 
