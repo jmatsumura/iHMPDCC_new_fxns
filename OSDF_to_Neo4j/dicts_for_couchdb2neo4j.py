da8e68012aa13d95990772f36792ae5e2d97ea97
James Matsumura <james.matsumura@gmail.com>
2016-10-18 17:09:52 -0400

    Fixing another naming inconsistency for schema vs what is in CouchDB. Need to expand out another dictionary that communicates what is at the end of a given edge.

--- OSDF_to_Neo4j/dicts_for_couchdb2neo4j.py
+++ OSDF_to_Neo4j/dicts_for_couchdb2neo4j.py
@@ -7,10 +7,13 @@ nodes = {
     'study': 'Study',
     'subject': 'Subject',
     'subject_attr': 'Subject_Attr',
+    'subject_attribute': 'Subject_Attr',
     'visit': 'Visit',
     'visit_attr': 'Visit_Attr',
+    'visit_attribute': 'Visit_Attr',
     'sample': 'Sample',
     'sample_attr': 'Sample_Attr',
+    'sample_attribute': 'Sample_Attr',
     'wgs_dna_prep': 'WGS_DNA_Prep',
     'host_seq_prep': 'Host_Seq_Prep',
     'wgs_raw_seq_set': 'WGS_Raw_Seq_Set',
@@ -21,7 +24,7 @@ nodes = {
     'viral_seq_set': 'Viral_Seq_Set',
     'annotation': 'Annotation',
     'clustered_seq_set': 'Clustered_Seq_set',
-    '16s_prep': '16S_Prep',
+    '16s_dna_prep': '16S_DNA_Prep',
     '16s_raw_seq_set': '16S_Raw_Seq_Set',
     '16s_trimmed_seq_set': '16S_Trimmed_Seq_Set',
     'microb_assay_prep': 'Microb_Assay_Prep',
@@ -45,5 +48,8 @@ edges = {
     'prepared_from': 'PREPARED_FROM',
     'sequenced_from': 'SEQUENCED_FROM',
     'derived_from': 'DERIVED_FROM',
-    'computed_from': 'COMPUTED_FROM'
+    'computed_from': 'COMPUTED_FROM',
+    'has_tag': 'HAS_TAG',
+    'has_mimarks': 'HAS_MIMARKS',
+    'has_mixs': 'HAS_MIXS'
 }
