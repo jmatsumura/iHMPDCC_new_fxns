c9ea9a459ad8e3e3b9340b48a6f20292b9060719
James Matsumura <james.matsumura@gmail.com>
2016-11-17 17:10:15 -0500

    New file for building all auto-complete fields/values.

---
+++ GQL_OSDF/autocomplete_map.py
@@ -0,0 +1,9 @@
+# This module contains all the information needed for populating the 
+# auto-complete field. Namely, the description, the doc_type, the
+# field, the full name, and the type of data.
+
+gql_map = {}
+
+gql_map['sample_fma_body_site'] = {"description": "The FMA body site the sample was derived from", "doc_type": "cases", "field": "Sample FMA body site", "full": "cases.Sample_fma_body_site", "type": "string"}
+gql_map['project_name'] = {"description": "The Project Name", "doc_type": "cases", "field": "ProjectName", "full": "cases.ProjectName", "type": "string"}
+gql_map['subject_gender'] = {"description": "Gender of subject", "doc_type": "cases", "field": "SubjectGender", "full": "cases.SubjectGender", "type": "string"}
